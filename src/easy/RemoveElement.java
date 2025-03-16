d5ea7ded0c4fc60739b3486b6033e476bcfd900b
karthikeyan-anandan-tw <karthikeyan.anandan@thoughtworks.com>
2025-03-16 16:22:54 +0530

    Added RemoveElement

---
+++ src/easy/RemoveElement.java
@@ -0,0 +1,36 @@
+package easy;
+
+/**
+ * <a href="https://leetcode.com/problems/remove-element/description/">...</a>
+ */
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 0,1,2,2,3,0,4,2 };
+        int val = 2;
+
+        int result = removeElement(nums, val);
+        System.out.println(result);
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int len = 0;
+
+        int numLen = nums.length;
+        int i = 0;
+        while (i < numLen) {
+            if (nums[i] != val) {
+                nums[len] = nums[i];
+                len++;
+            }
+            i++;
+        }
+
+        for (int j = 0; j < len; j++) {
+            System.out.println(nums[j]);
+        }
+
+        return len;
+    }
+
+}
