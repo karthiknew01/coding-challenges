4f4fab8af8f5f7e5327f97647a01fd3c3b348e7b
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-03-19 23:45:28 +0530

    Create AddBinary

---
+++ src/easy/AddBinary.java
@@ -0,0 +1,1 @@
+int i = a.length() - 1;int j = b.length() - 1;StringBuilder binary = new StringBuilder();int carry = 0;while(i >= 0 || j >= 0) { int aInt = i >= 0 ? a.charAt(i--) - 48 : 0; int bInt = j >= 0 ? b.charAt(j--) - 48 : 0; carry = aInt + bInt + carry; binary.append(carry % 2); carry /= 2;}if (carry == 1) binary.append(carry);return binary.reverse().toString();
