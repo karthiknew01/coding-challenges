4429b2ddfa56cbf269eed147a0cf8a3f6a04944e
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-03-25 19:09:39 +0530

    Added RemoveDuplicatesSortedList

---
+++ src/easy/RemoveDuplicatesSortedList.java
@@ -0,0 +1,34 @@
+package easy;
+
+import medium.helper.ListNode;
+
+/**
+ * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/">...</a>
+ */
+public class RemoveDuplicatesSortedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(3);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(3);
+
+        head = deleteDuplicates(head);
+        head.printList();
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return head;
+
+        ListNode temp = head;
+
+        while (head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        return temp;
+    }
+}
