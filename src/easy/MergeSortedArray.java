ea21b14aec13b5a1b3c1e8cdd519c160134d7da6
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-04-05 00:19:22 +0530

    Added MergeSortedArray

---
+++ src/easy/MergeSortedArray.java
@@ -0,0 +1,26 @@
+package easy;
+
+/**
+ * <a href="https://leetcode.com/problems/merge-sorted-array/description/">...</a>
+ */
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0}, nums2 = {2,5,6};
+        int m = 3,  n = 3;
+
+        merge(nums1, m, nums2, n);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0)
+            nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+        while (j >= 0)
+            nums1[k--] = nums2[j--]; // Copy remaining nums2 elements
+
+        for (int num: nums1) {
+            System.out.println(num);
+        }
+    }
+}
