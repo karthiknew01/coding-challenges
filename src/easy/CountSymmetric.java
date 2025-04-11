d29c5968ffb5c5eb19da177475f0f91dc36845af
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-04-11 21:24:55 +0530

    Added CountSymmetric

---
+++ src/easy/CountSymmetric.java
@@ -0,0 +1,36 @@
+package easy;
+
+/**
+ * <a href="https://leetcode.com/problems/count-symmetric-integers/description/?envType=daily-question&envId=2025-04-11">...</a>
+ */
+public class CountSymmetric {
+
+    public static void main(String[] args) {
+        System.out.println(countSymmetricIntegers(1200,1230));
+    }
+
+    public static int countSymmetricIntegers(int low, int high) {
+        int symmetric = 0;
+
+        low = Math.max(low, 11);
+        for (int i = low; i <= high; i++) {
+            String val = Integer.toString(i);
+            int length = val.length();
+            if (length % 2 == 0 && checkSum(val))
+                symmetric++;
+        }
+
+        return symmetric;
+    }
+
+
+    public static boolean checkSum(String s) {
+        int sum = 0;
+        int mid = s.length() / 2;
+        for (int i = 0; i < mid; i++) {
+            sum += s.charAt(i) - s.charAt(i + mid);
+        }
+        return sum == 0;
+    }
+
+}
