cd1b41e6ad7c42192c9b3cf64e8bf6045ed583ce
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-04-29 17:42:02 +0530

    Added CountSubArrayWhereMaxElementAppearsAtLeastKTimes

---
+++ src/medium/CountSubArrayWhereMaxElementAppearsAtLeastKTImes.java
@@ -0,0 +1,37 @@
+package medium;
+
+/**
+ * <a href="https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/?envType=daily-question&envId=2025-04-28">...</a>
+ */
+public class CountSubArrayWhereMaxElementAppearsAtLeastKTImes {
+
+    public static void main(String[] args) {
+        int[] nums = {28,5,58,91,24,91,53,9,48,85,16,70,91,91,47,91,61,4,54,61,49};
+                int k = 1;
+        System.out.println(countSubarrays(nums, k));
+    }
+
+    public static long countSubarrays(int[] nums, int k) {
+        long res = 0;
+        int left = 0;
+        int n = nums.length;
+        int max = 0;
+        int maxCount = 0;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        for (int right = 0; right < n; right++) {
+            if(nums[right] == max) maxCount++;
+
+            while (maxCount == k) {
+                res += n - right;
+                if(nums[left++] == max)
+                    maxCount--;
+            }
+        }
+
+        return res;
+    }
+}
