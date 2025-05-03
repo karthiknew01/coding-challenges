3e453703894a43dfba046908140e0a5a3c7028ad
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-05-03 23:59:31 +0530

    Added BestTimeToBuyAndSellStock

---
+++ src/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,32 @@
+package easy;
+
+/**
+ * <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/">...</a>
+ */
+public class BestTimeToBuyAndSellStock {
+
+    public static void main(String[] args) {
+        int[] prices = {7,6,4,3,1};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int left = 0;
+        int maxProfit = 0;
+
+        for (int right = 0; right < prices.length; right++) {
+            if (prices[left] < prices[right]) {
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                left = right;
+            }
+
+        }
+
+        return maxProfit;
+    }
+
+
+
+}
