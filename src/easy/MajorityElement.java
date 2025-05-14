5916b02da09cc98cd70bbe8a03f8208abd0f52d0
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-05-14 18:42:35 +0530

    Added MajorityElement

---
+++ src/easy/MajorityElement.java
@@ -0,0 +1,35 @@
+package easy;
+
+public class MajorityElement {
+
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,3};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int count = 0, candidate = -1;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else {
+                if (num == candidate)
+                    count++;
+                else
+                    count--;
+            }
+        }
+
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate)
+                count++;
+        }
+        if (count > (nums.length / 2))
+            return candidate;
+        return -1;
+    }
+}
