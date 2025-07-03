7841908c63ae78ae7e78da21b951600d3579607b
Karthikeyan Anandan <karthiknew01@gmail.com>
2025-07-03 10:02:30 +0530

    Added FindKthChar

---
+++ src/easy/FindKthChar.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class FindKthChar {
+
+    public static void main(String[] args) {
+        int k = 5;
+        System.out.println(kthCharacter(k));
+    }
+
+    public static char kthCharacter(int k) {
+        String generatedString = "abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij";
+        return generatedString.charAt(k - 1);
+    }
+
+}
